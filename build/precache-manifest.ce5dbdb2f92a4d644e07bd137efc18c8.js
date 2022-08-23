42e3d10bdee950d4cddb1be7f7c46cd8bd70e6cc
Simant-a <87609037+Simant-a@users.noreply.github.com>
2022-08-23 15:29:11 +0530

    Add files via upload

---
+++ build/precache-manifest.ce5dbdb2f92a4d644e07bd137efc18c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e622bdfbc292595fefc23fe517473d9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6cfe5981ab3ebc8b57f",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cfe5981ab3ebc8b57f",
+    "url": "/static/js/main.b19cc14d.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
